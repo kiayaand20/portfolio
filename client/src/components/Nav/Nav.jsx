0d8364e0bf727cf49f6c6c6c6e3ecff8ef98834f
Kiaya Anderson <kiayaanderson@Kiayas-Air.lan>
2022-02-09 15:15:37 -0500

    updated font on home page

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -8,7 +8,7 @@ function Nav() {
       <Link to='/about' className='about-link'>About</Link>
       <Link to='/projects' className='project-link'>Projects</Link>
       <Link to='/contact' className='contact-link'>Contact</Link>
-      <a href="https://drive.google.com/file/d/1_wAXoveW9AjQV8RC1e-s24ftp-cO2mFW/view"
+      <a href="https://drive.google.com/file/d/1_wAXoveW9AjQV8RC1e-s24ftp-cO2mFW/view?usp=sharing"
         rel="noreferrer" target="_blank" className='resume-link'><Link to='/resume'>
         </Link>Resume</a>
     </nav>
