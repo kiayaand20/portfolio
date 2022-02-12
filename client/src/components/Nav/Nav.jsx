ea40ec04cc0ce5b857f6b93bd3d74e828016e808
Kiaya Anderson <kiayaanderson@Kiayas-Air.lan>
2022-02-12 12:44:19 -0500

    added line under skills section

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -16,7 +16,7 @@ const alwaysOptions = (
     <NavLink className="link" to="/contact">
       Contact
     </NavLink>
-    <a href="https://drive.google.com/file/d/18zqJBot16OykCqDeufbUY07sJUzKss5O/view?usp=sharing"
+    <a href="https://drive.google.com/file/d/1TIFXeMVeePE0vuVd3Zn4pOtK7NuMlSNV/view?usp=sharing"
         rel="noreferrer" target="_blank" className='link'><Link to='/resume'>
         </Link>Resume</a>
   </>
