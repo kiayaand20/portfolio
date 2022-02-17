5cd07354ee6955168664a9a250979732977fc54b
Kiaya Anderson <kiayaanderson@Kiayas-Air.lan>
2022-02-17 05:49:19 -0500

    made an updarte to the nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -16,7 +16,7 @@ const alwaysOptions = (
     <NavLink className="link" to="/contact">
       Contact
     </NavLink>
-    <a href="https://drive.google.com/file/d/13jgpfcy4eNsqIptybcnDOxUJSVNnY7aV/view?usp=sharing"
+    <a href="https://drive.google.com/file/d/1fWdBG9eqjvg_PpoHSi661AnVPPVSk58d/view?usp=sharing"
         rel="noreferrer" target="_blank" className='link'><Link to='/resume'>
         </Link>Resume</a>
   </>
